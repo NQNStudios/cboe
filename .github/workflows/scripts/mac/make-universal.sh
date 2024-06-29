20ac8b420cda7c649e133af2f398a77dfdb8cca1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-06-29 13:45:23 -0600

    parent 9548369023262114cd87de9df9b3bc2b8a533084 author Nat Quayle Nelson <natquaylenelson@gmail.com> 1719690323 -0600 committer Nat Quayle Nelson <natquaylenelson@gmail.com> 1730340833 -0500
    Release build squashed

---
+++ .github/workflows/scripts/mac/make-universal.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ -z "$CONFIGURATION" ]; then
+    CONFIGURATION=Release
+fi
+
+INTEL=cboe-macos-intel-$CONFIGURATION
+SILICON=cboe-macos-silicon-$CONFIGURATION
+
+combine() {
+    mkdir -p "build/Blades of Exile/$1.app/Contents/MacOS"
+    lipo -create "$SILICON/Blades of Exile/$1.app/Contents/MacOS/$1" "$INTEL/Blades of Exile/$1.app/Contents/MacOS/$1" -output "build/Blades of Exile/$1.app/Contents/MacOS/$1"
+    cp -r "$SILICON/Blades of Exile/$1.app/Contents/Frameworks" "build/Blades of Exile/$1.app/Contents/"
+    cp -r "$SILICON/Blades of Exile/$1.app/Contents/Resources" "build/Blades of Exile/$1.app/Contents/"
+    cp "$SILICON/Blades of Exile/$1.app/Contents/Info.plist" "build/Blades of Exile/$1.app/Contents/"
+    cp "$SILICON/Blades of Exile/$1.app/Contents/PkgInfo" "build/Blades of Exile/$1.app/Contents/"
+}
+
+combine "Blades of Exile"
+combine "BoE Scenario Editor"
+combine "BoE Character Editor"
