3f5234ffbcf9dbfaf6d8eed3fbee500fd0f2a46b
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-07-02 12:12:25 -0600

    ls the directories

--- .github/workflows/scripts/mac/scons-build.sh
+++ .github/workflows/scripts/mac/scons-build.sh
@@ -4,5 +4,4 @@ export CC="$(brew --prefix llvm)/bin/clang"
 export CXX="$(brew --prefix llvm)/bin/clang++"
 export SDKROOT="$(xcrun --show-sdk-path)"
 
-echo $@
 scons CXXFLAGS="-I/usr/local/opt/zlib/include" LINKFLAGS="-L/usr/local/opt/zlib/lib" $@
