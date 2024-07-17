a23ff469fa7ecdd17ab7dd73a1ed37e96a08eda6
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-07-17 17:11:50 -0600

    zlib-build.bat

---
+++ .github/workflows/scripts/win/zlib-build.bat
@@ -0,0 +1,9 @@
+
+vcpkg integrate install
+
+setlocal enabledelayedexpansion
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+	"%%i" -clp:ForceConsoleColor -target:Build -property:Configuration=Release -property:Platform=%1 -property:VcpkgEnableManifest=true "deps/zlib/build/zlib.sln"
+	exit /b !errorlevel!
+)
