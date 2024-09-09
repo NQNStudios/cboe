249fb986e4845da22b0946cb0401e1515763e8ad
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-09 17:11:23 -0500

    don't pass arch flags, we're doing this the stupid way

--- .github/workflows/scripts/mac/scons-build.sh
+++ .github/workflows/scripts/mac/scons-build.sh
@@ -4,4 +4,4 @@ export CC="$(brew --prefix llvm)/bin/clang"
 export CXX="$(brew --prefix llvm)/bin/clang++"
 export SDKROOT="$(xcrun --show-sdk-path)"
 
-scons CXXFLAGS="-I/usr/local/opt/zlib/include -arch x86_64 -arch arm64" LINKFLAGS="-L/usr/local/opt/zlib/lib" $@
+scons CXXFLAGS="-I/usr/local/opt/zlib/include" LINKFLAGS="-L/usr/local/opt/zlib/lib" $@
