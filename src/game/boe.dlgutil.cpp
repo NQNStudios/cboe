9e7dd181cd86a3fa08c604dc55ed5895f7a5135c
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-02-16 10:25:04 -0600

    Make getting scenario strings DRY
    I changed the name of get_strs() because it seemed
    too similar to get_str(), which refers to the resource
    string lists. These functions are getting scenario
    things--there shouldn't be ambiguity there.

--- src/game/boe.dlgutil.cpp
+++ src/game/boe.dlgutil.cpp
@@ -1113,7 +1113,7 @@ void handle_talk_node(int which_talk_entry) {
 				save_talk_str1 = s1 >= 0 ? univ.town->spec_strs[s1] : "";
 				save_talk_str2 = s2 >= 0 ? univ.town->spec_strs[s2] : "";
 			}
-			get_strs(save_talk_str1, save_talk_str2, eSpecCtxType::TOWN, s1, s2);
+			get_scen_strs(save_talk_str1, save_talk_str2, eSpecCtxType::TOWN, s1, s2);
 			put_pc_screen();
 			put_item_screen(stat_window);
 			break;
@@ -1124,7 +1124,7 @@ void handle_talk_node(int which_talk_entry) {
 				save_talk_str1 = s1 >= 0 ? univ.scenario.spec_strs[s1] : "";
 				save_talk_str2 = s2 >= 0 ? univ.scenario.spec_strs[s2] : "";
 			}
-			get_strs(save_talk_str1, save_talk_str2, eSpecCtxType::SCEN, s1, s2);
+			get_scen_strs(save_talk_str1, save_talk_str2, eSpecCtxType::SCEN, s1, s2);
 			put_pc_screen();
 			put_item_screen(stat_window);
 			break;
