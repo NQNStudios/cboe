978f061481416d947387238d1597ec6253f78aa5
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-09-15 09:33:37 -0500

    test butler early in CI

--- .github/workflows/scripts/butler_push.sh
+++ .github/workflows/scripts/butler_push.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Pass any argument to make it a dry run (for putting at the start of your CI, to catch failure early)
+test="${1}"
+
 butler_channel=""
 butler_exe=""
 release_dir=""
@@ -30,11 +33,16 @@ fi
 # Butler download is disabled because something is wrong with the automated download links
 ## -L follows redirects
 ## -O specifies output name
-# curl -L -o butler.zip https://broth.itch.ovh/butler/${butler_channel}/LATEST/archive/default
-# unzip butler.zip
+# if [ ! -f ./${butler_exe} ]; then
+#   curl -L -o butler.zip https://broth.itch.ovh/butler/${butler_channel}/LATEST/archive/default
+#   unzip butler.zip
+# fi
 
 # GNU unzip tends to not set the executable bit even though it's set in the .zip
 chmod +x ${butler_exe}
 # just a sanity check run (and also helpful in case you're sharing CI logs)
 ./${butler_exe} -V
-./${butler_exe} push "build/Blades of Exile/" nqn/blades-of-exile:${release_dir}
+
+if [ -z "$test" ]; then
+    ./${butler_exe} push "build/Blades of Exile/" nqn/blades-of-exile:${release_dir}
+fi
